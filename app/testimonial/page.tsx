c187358bc57cded66cdaac6f825a6df99dfd879b
Michal Špitálský <michal.spitalsky.mp@gmail.com>
2024-11-17 17:58:42 +0000

    feat: testimonial

--- app/testimonial/page.tsx
+++ app/testimonial/page.tsx
@@ -2,29 +2,21 @@
 
 import React, { useRef, useState } from "react";
 import Image from "next/image";
-import Link from "next/link";
 
 import { toPng } from "html-to-image";
 
-import { formatDate, formatTime } from "@/utils/formatters";
-import {
-  bgColorsValidate,
-  imageUploadValidate,
-  lengthValidate,
-} from "@/utils/validators";
-import { accents } from "@/datasets/colors";
+import { imageUploadValidate, lengthValidate } from "@/utils/validators";
 import { facilities } from "@/datasets/facilities";
 import ErrorText from "@/components/error-text";
 
 const TestimonialGenerator = () => {
   //--- STATES AND REFS --------------------------------------------------------------
-  const [eventType, setEventType] = useState("");
-  const [heading, setHeading] = useState("");
-  const [description, setDescription] = useState("");
+
+  const [author, setAuthor] = useState("");
+  const [text, setText] = useState("");
 
   const [image1, setImage1] = useState<File | null>(null);
 
-  const [facility, setFacility] = useState(0);
   const previewRef = useRef<HTMLDivElement>(null);
 
   //--- UPLOAD FUNCTION --------------------------------------------------------------
@@ -38,18 +30,17 @@ const TestimonialGenerator = () => {
 
   //--- VALIDATOR VARIABLES --------------------------------------------------------------
 
-  const descLengthLong = 180;
+  const descLengthLong = 200;
 
   //--- VALIDATOR FUNCTIONS --------------------------------------------------------------
   const descLengthValidate = lengthValidate(
-    description.length,
+    text.length,
     descLengthLong,
     descLengthLong,
     false
   );
-  const areImagesUploaded = imageUploadValidate(1, image1);
-  const isHeadingSet = heading !== "";
-  const isDescSet = description !== "";
+
+  const isTextSet = text !== "";
 
   //--- IMAGE GENERATOR --------------------------------------------------------------
   const handleGenerate = async () => {
@@ -60,7 +51,7 @@ const TestimonialGenerator = () => {
       });
       const link = document.createElement("a");
       link.download =
-        "SFÉRA_1080x1080px_" + heading.replace(" ", "-") + "-prispevek.png";
+        "SFÉRA_1080x1080px_" + author.replace(" ", "-") + "-prispevek.png";
       link.href = pngData;
       link.click();
     }
@@ -70,40 +61,18 @@ const TestimonialGenerator = () => {
     <div className="p-4 h-auto flex flex-row">
       <div className="w-1/2 p-10 border-r">
         {/* --- INPUT SECTION -------------------------------------------------------------- */}
-        <h1 className="text-xl font-bold mb-4">
-          Generátor: Oznámení s obrázkem
-        </h1>
+        <h1 className="text-xl font-bold mb-4">Generátor: Reference</h1>
 
         {/* --- NAME, TYPE AND DESCRIPTION SECTION --- */}
         <div className="w-full py-5 border-b">
-          <div className="flex flex-row flex-nowrap w-full gap-5">
-            <div className="mb-4 w-full">
-              <label className="block font-semibold">
-                Název akce<span className="text-sos"> *</span>
-              </label>
-              <div className="relative">
-                <input
-                  type="text"
-                  value={heading}
-                  onChange={(e) => setHeading(e.target.value)}
-                  className="border p-2 w-full"
-                  maxLength={36}
-                />
-                <p className="absolute text-slate-400 right-3 top-2">
-                  {36 - heading.length}
-                </p>
-              </div>
-            </div>
-          </div>
-
           <div className="mb-4">
             <label className="block font-semibold">
               Popisek<span className="text-sos"> *</span>
             </label>
             <div className="relative">
               <textarea
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
+                value={text}
+                onChange={(e) => setText(e.target.value)}
                 className="border p-2 w-full relative"
                 style={{
                   borderColor: descLengthValidate ? "" : "red",
@@ -114,15 +83,32 @@ const TestimonialGenerator = () => {
                 className="absolute right-3 top-2 text-slate-400"
                 style={{ color: descLengthValidate ? "" : "var(--sos)" }}
               >
-                {descLengthLong - description.length}
+                {descLengthLong - text.length}
               </p>
               {!descLengthValidate ? (
                 <ErrorText>
-                  <>Prosím, zkrať popisek, nebo odeber datum a čas</>
+                  <>Prosím, zkrať referenci</>
                 </ErrorText>
               ) : (
                 <></>
               )}
+            </div>
+          </div>{" "}
+          <div className="flex flex-row flex-nowrap w-full gap-5">
+            <div className="mb-4 w-full">
+              <label className="block font-semibold">Jméno citovaného</label>
+              <div className="relative">
+                <input
+                  type="text"
+                  value={author}
+                  onChange={(e) => setAuthor(e.target.value)}
+                  className="border p-2 w-full"
+                  maxLength={50}
+                />
+                <p className="absolute text-slate-400 right-3 top-2">
+                  {50 - author.length}
+                </p>
+              </div>
             </div>
           </div>
         </div>
@@ -133,7 +119,7 @@ const TestimonialGenerator = () => {
             <div className="w-1/2">
               <div className="mb-4">
                 <label className="block font-semibold">
-                  Nahrajte první prvek: <span className="text-sos"> *</span>
+                  Nahrajte autorovu fotografii:
                 </label>
                 <input
                   type="file"
@@ -143,29 +129,6 @@ const TestimonialGenerator = () => {
               </div>
             </div>
           </div>
-          <div className="bg-accent-pink p-5 mt-5">
-            <p>
-              Do této šablony povoluji vkládat fotky a obrázky. Nesmí se tu ale
-              objevit nic s textem (například bannery, nebo cizí plakáty). Kdo
-              tam něco takového dá, tomu utrhnu hlavu.
-            </p>
-          </div>
-        </div>
-
-        {/* --- FACILITY SECTION --- */}
-        <div className="w-full py-5 border-b">
-          <label className="block font-semibold">Kdo zaštiťuje akci?</label>
-          <select
-            onChange={(e) => setFacility(parseInt(e.target.value))}
-            className="border-2 px-6 py-1"
-          >
-            <option value={0}>Nechat prázdné</option>
-            {facilities.map((facility) => (
-              <option value={facility.id} key={facility.name}>
-                {facility.name}
-              </option>
-            ))}
-          </select>
         </div>
 
         {/* --- DOWNLOAD SECTION --- */}
@@ -173,12 +136,8 @@ const TestimonialGenerator = () => {
           {
             // Is the text not overflowing?
             descLengthValidate &&
-            // Are the images uploaded?
-            areImagesUploaded &&
-            // Is the title set?
-            isHeadingSet &&
             // Is the description set?
-            isDescSet ? (
+            isTextSet ? (
               <button
                 onClick={handleGenerate}
                 className="mt-4 border-2 border-black px-8 py-4 font-bold"
@@ -190,19 +149,8 @@ const TestimonialGenerator = () => {
                 <button className="my-4 border-2 border-gray-400 bg-gray-300 px-8 py-4 font-bold text-gray-400">
                   Stáhnout příspěvek (.png)
                 </button>
-
-                {!isHeadingSet ? (
-                  <ErrorText>Prosím, vyplň název události</ErrorText>
-                ) : (
-                  <></>
-                )}
-                {!isHeadingSet ? (
-                  <ErrorText>Prosím, vyplň krátký popisek</ErrorText>
-                ) : (
-                  <></>
-                )}
-                {!areImagesUploaded ? (
-                  <ErrorText>Prosím, nahraj obrázek</ErrorText>
+                {!isTextSet ? (
+                  <ErrorText>Prosím, vyplň referenci</ErrorText>
                 ) : (
                   <></>
                 )}
@@ -217,7 +165,7 @@ const TestimonialGenerator = () => {
         <div className="scale-50" style={{ transformOrigin: "center 25%" }}>
           <div
             ref={previewRef}
-            className="relative pointer-events-none border bg-white flex flex-row flex-nowrap announcement"
+            className="relative pointer-events-none border bg-white flex flex-row flex-nowrap testimonial"
             style={{ width: "1080px", height: "1080px" }}
           >
             <div className="w-[100px] h-full border-black border-r-2">
@@ -227,51 +175,27 @@ const TestimonialGenerator = () => {
             <div className="w-[880px] h-full border-black border-r-2">
               <div className="h-[100px] w-full border-black border-b-2"></div>
               <div className="h-[880px] w-full border-black border-b-2 flex flex-col overflow-hidden">
-                <div className="flex flex-row flex-nowrap">
-                  <div className="w-[880px] h-[440px] border-black border-b-2 relative">
-                    {image1 && (
+                <div className="mx-[120px] h-full flex flex-col py-[20px] justify-evenly">
+                  {image1 && (
+                    <div className="w-40 h-40 relative overflow-hidden mx-auto rounded-full">
                       <Image
                         src={URL.createObjectURL(image1)}
                         alt="Image 1"
-                        className="object-cover"
                         fill
+                        className="object-cover"
                       />
-                    )}
-                  </div>
-                </div>
-                {facility !== 0 ? (
-                  <div
-                    className="w-full h-32 border-b-2 border-black flex flex-row justify-center items-center text-center"
-                    style={{
-                      backgroundColor:
-                        "var(--" +
-                        facilities[facility - 1].colorBgVarName +
-                        ")",
-                    }}
-                  >
-                    <p className="facility text-white">
-                      {facilities[facility - 1].name}
-                    </p>
-                  </div>
-                ) : (
-                  <></>
-                )}
-                <div className="mx-[60px] h-full flex flex-col justify-around py-[20px]">
-                  <div>
-                    {heading ? (
-                      <h2 className="main-heading">{heading}</h2>
-                    ) : (
-                      <></>
-                    )}
-                  </div>
-                  <p
-                    className="desc m-0"
-                    style={{
-                      maxHeight: "180px",
-                    }}
-                  >
-                    {description}
-                  </p>
+                    </div>
+                  )}
+                  <p className="desc m-0">{text}</p>
+                  {author && (
+                    <div>
+                      {author ? (
+                        <h2 className="author">&mdash; {author}</h2>
+                      ) : (
+                        <></>
+                      )}
+                    </div>
+                  )}
                 </div>
               </div>
             </div>
@@ -286,4 +210,4 @@ const TestimonialGenerator = () => {
   );
 };
 
-export default AnnouncementImageGenerator;
+export default TestimonialGenerator;
