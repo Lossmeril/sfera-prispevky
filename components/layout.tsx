b554b3ab9f0a966d5da7184a79c2dfff6630373c
Michal Špitálský <michal.spitalsky@pm.me>
2025-10-27 11:52:30 +0100

    feat (*): editor sections made into components

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,3 +1,23 @@
-interface PreviewSectionProps {
+interface SectionProps {
   children: React.ReactNode;
 }
+
+export const MenuSection: React.FC<SectionProps> = ({ children }) => {
+  return (
+    <div className="w-full lg:w-1/2 border-r border-black">{children}</div>
+  );
+};
+
+export const MenuBlock: React.FC<SectionProps> = ({ children }) => {
+  return (
+    <div className="w-full border-b border-black px-10 py-4">{children}</div>
+  );
+};
+
+export const PreviewSection: React.FC<SectionProps> = ({ children }) => {
+  return (
+    <div className="w-full lg:w-1/2 pointer-events-none max-h-screen overflow-hidden p-10 bg-gray-200">
+      {children}
+    </div>
+  );
+};
