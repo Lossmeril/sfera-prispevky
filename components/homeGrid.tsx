91f4ad269394f99d454ff0a261344cedc5fa3d9f
Michal Špitálský <michal.spitalsky@pm.me>
2025-11-04 12:02:18 +0100

    style (home): home grid content and styling

--- components/homeGrid.tsx
+++ components/homeGrid.tsx
@@ -20,12 +20,12 @@ const GeneratorGrid = ({ title, items }: GeneratorGridProps) => {
       <h2 className="text-xl md:text-2xl xl:text-3xl font-bold text-left leading-none border border-black w-full p-4 border-b-0">
         {title}
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 mb-20 w-full border-y border-black">
-        {items.map((item) => (
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 mb-20 w-full border-t border-black">
+        {items.map((item, index) => (
           <Link
             href={item.link}
-            className="hover:grayscale transition-all border-l border-black w-full overflow-hidden aspect-[3/4] flex flex-col"
-            key={item.link}
+            className="hover:grayscale transition-all border-l border-b border-black w-full overflow-hidden aspect-[3/4] flex flex-col"
+            key={index}
           >
             <div className="w-full aspect-square border-b border-black relative bg-gray-100 grid place-content-center">
               <Image
@@ -47,7 +47,7 @@ const GeneratorGrid = ({ title, items }: GeneratorGridProps) => {
         {emptySpaces.map((_, index) => (
           <div
             key={`empty-${index}`}
-            className="border-l last-of-type:border-r border-black w-full overflow-hidden aspect-[3/4] bg-white hidden lg:block"
+            className="border-l last-of-type:border-r border-b border-black w-full overflow-hidden aspect-[3/4] bg-white hidden lg:block"
           ></div>
         ))}
       </div>
--- datasets/generatableImages.tsx
+++ datasets/generatableImages.tsx
@@ -5,11 +5,12 @@ type GeneratorType = {
 
 export const generatablePosts: GeneratorType[] = [
   { name: "Příspěvek s jedním prvkem", link: "/posts/one-element" },
-  { name: "Příspěvek se dvěma prvky", link: "/posts/two-elements" },
+  { name: "Příspěvek se dvěma prvky", link: "" },
+  { name: "Příspěvek se čtyřmi prvky", link: "" },
 ];
 
 export const generatableScreens: GeneratorType[] = [
-  { name: "Obrazovka se čtyřmi prvky", link: "" },
+  { name: "Obrazovka se čtyřmi prvky", link: "/screens/four-elements" },
 ];
 
 export const generatableWebThumbs: GeneratorType[] = [
@@ -17,6 +18,9 @@ export const generatableWebThumbs: GeneratorType[] = [
 ];
 
 export const generatablePosters: GeneratorType[] = [
-  { name: "Plakátek se dvěma prvky", link: "" },
   { name: "Informační cedule", link: "/posters/sign" },
+  { name: "Plakátek A4 s jedním prvkem", link: "" },
+  { name: "Plakátek A4 se dvěma prvky", link: "" },
+  { name: "Plakátek A4 s obrázkem a prvkem", link: "" },
+  { name: "Plakátek A4 s obrázkem a QR kódem", link: "" },
 ];
